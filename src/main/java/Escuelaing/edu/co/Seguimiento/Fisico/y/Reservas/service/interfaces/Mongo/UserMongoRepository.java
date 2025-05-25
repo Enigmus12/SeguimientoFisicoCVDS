36e9cd79fbc3160a4a84c60c63eea4979f74cd63
Juan <Jdrorodriguez2@gmail.com>
2025-05-25 13:33:58 -0500

    Nuevo endpoint para usuario

--- src/main/java/Escuelaing/edu/co/Seguimiento/Fisico/y/Reservas/service/interfaces/Mongo/UserMongoRepository.java
+++ src/main/java/Escuelaing/edu/co/Seguimiento/Fisico/y/Reservas/service/interfaces/Mongo/UserMongoRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserMongoRepository extends MongoRepository<User, String> {
     User findByUserName(String userName);
+    User findByNumberId(Integer numberId);
 }
